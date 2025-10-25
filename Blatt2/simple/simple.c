efaa09ac34bd56e8f0b2120e0e33d214bc4324da
GitJKMN <noel.pawelczyk@studium.uni-hamburg.de>
2025-10-25 16:52:44 +0200

    Aufgabe 1 bearbeitet

--- Blatt2/simple/simple.c
+++ Blatt2/simple/simple.c
@@ -9,35 +9,35 @@
 
 int *mistakes1(void) {
   int buf[] = {1, 1, 2, 3, 4, 5};
-  int *buf_ptr = malloc(sizeof(buf));
-  memcpy(buf_ptr, buf, sizeof(buf));
+  int *buf_ptr = malloc(sizeof(buf)); //Explizite Allokierung von Speicher
+  memcpy(buf_ptr, buf, sizeof(buf));  //int-Array buf wird aus dem Stack-Frame in den allokierten Speicher kopiert
   return buf_ptr;
 }
 
 int *mistakes2(void) {
-  int *buf = malloc(sizeof(int) * 4);
-  buf[1] = 2;
+  int *buf = malloc(sizeof(int) * 4); //allokierter Speicher muss an die Größe von int angepasst werden
+  buf[1] = 2;                         //*p[1] == &mistakes2()[1] ruft zweite Stelle des Arrays auf => buf[1]
   return buf;
 }
 
 int *mistakes3(void) {
   /* In dieser Funktion darf kein Speicher direkt d.h. explizit allokiert werden. */
   int mistakes2_ = 0;
-  int *buf = mistakes2();
+  int *buf = mistakes2(); //implizite Allokierung von Speicher über mistakes2
   buf[mistakes2_] = 3;
   return buf;
 }
 
 int *mistakes4(void) {
-  int *buf = malloc(sizeof(int) * 4);
-  buf[0] = 4;
+  int *buf = malloc(sizeof(int) * 4); //parallel zu mistakes1()
+  buf[0] = 4;                         //*p[3] == mistakes4() ruft erste Stelle des Arrays auf => buf[0]
   //free(buf);
   return buf;
 }
 
 int *mistakes5(void) {
-  int *buf = malloc(5 * sizeof(int));
-  buf[4] = 5;
+  int *buf = malloc(5 * sizeof(int)); //parallel zu mistakes1()
+  buf[4] = 5;                         //muss an mistakes5()+4 angepasst werden => buf[4]
   return buf;
 }
 
@@ -54,6 +54,7 @@ int main(void) {
 
   /* TODO */
   /* Fügen sie hier die korrekten aufrufe von free() ein */
+  //free() muss auf die allgemeine Array-Referenz angewendet werden 
   free(p[0] - 1); 
   free(p[1] - 1);
   free(p[2]);
