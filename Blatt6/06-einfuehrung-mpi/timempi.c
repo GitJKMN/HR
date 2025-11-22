1a19cf19f84d7f993f559aa5a1a21dda0d580b1f
JKMN <noelpawelczyk@gmail.com>
2025-11-22 17:32:00 +0100

    Aufgabe 1 und 2 bearbeitet

--- Blatt6/06-einfuehrung-mpi/timempi.c
+++ Blatt6/06-einfuehrung-mpi/timempi.c
@@ -5,27 +5,61 @@
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
+#include "mpi.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
-  struct timeval tv;
-  time_t current_time;
-  int micro_sec;
-  char time_string[30];
-  char output[80];
-  char hostname[30];
 
-  gettimeofday(&tv, NULL);
-  gethostname(hostname, 30);
+  int rank, size;
+  MPI_Init( &argc, &argv ); 
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+  MPI_Comm_size( MPI_COMM_WORLD, &size );
+  
+  int master_rank = size - 1;
+  int is_master = rank == master_rank;
 
-  current_time = tv.tv_sec;
-  micro_sec = tv.tv_usec;
+  if (is_master) {
+    int ms[master_rank];
+    int min_ms;
+    int max_ms;
+    for (int i = 0; i < master_rank; i++) {
+      char recv[80];
+      MPI_Recv(&recv, 80, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&ms[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      printf("%s\n", recv);
+    }
+    min_ms = ms[0];
+    max_ms = ms[0];
+    for (int i = 0; i < master_rank; i++) {
+      if (ms[i] < min_ms) {
+        min_ms = ms[i];
+      } else if (ms[i] > max_ms) {
+        max_ms = ms[i];
+      }
+    }
+    printf("[%d] Kleinster MS-Anteil: %d\n",rank, min_ms);
+    printf("[%d] Größte Differenz: %d\n", rank, max_ms - min_ms);
+  } else {
+    struct timeval tv;
+    time_t current_time;
+    int micro_sec;
+    char time_string[30];
+    char output[80];
+    char hostname[30];
 
-  strftime(time_string, 30, "%Y-%m-%d %T", localtime(&current_time));
-  snprintf(output, 80, "[%d] %s // %s.%d", -1, hostname, time_string, micro_sec);
+    gettimeofday(&tv, NULL);
+    gethostname(hostname, 30);
 
-  printf("%s\n", output);
-  printf("%d\n", micro_sec);
+    current_time = tv.tv_sec;
+    micro_sec = tv.tv_usec;
 
+    strftime(time_string, 30, "%Y-%m-%d %T", localtime(&current_time));
+    snprintf(output, 80, "[%d] %s // %s.%d", rank, hostname, time_string, micro_sec);
+    MPI_Send(&output, 80, MPI_CHAR, master_rank, 0, MPI_COMM_WORLD);
+    MPI_Send(&micro_sec, 1, MPI_INT, master_rank, 0, MPI_COMM_WORLD);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  MPI_Finalize();
+  printf("[%d] beendet jetzt!\n", rank);
   return 0;
 }
